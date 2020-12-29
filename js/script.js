1c8363a03b4d547fbaabedaacee3b67efeac2d26
thuannguyen <phuthuan.ng@gmail.com>
2020-12-29 16:14:02 +0700

    Init

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var loaiXe = $('input[name="loaixe"]').val();
+var km = $('input[name="km"]').val();
+var thoigian = $('input[name="thoigian"]').val();
+
+$('.btn-tinhtien').on('click', function(){
+    alert(loaiXe);
+});
